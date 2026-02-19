365ee5446c00cacb52a32785432ae69bd2372dd8
dhondikarthikeya1 <dhondikarthikeya1@gmail.com>
2026-02-19 20:11:00 +0530

    Fix vercel SPA rewrite for react-router

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -1,7 +1,7 @@
 export const SITE = {
   name: "Inspire College of Hotel Management",
   location: "Armoor, Nizamabad",
-  phone: "91+ 8374382391",
-  whatsapp: "8374382391", // without +
+  phone: "91+ 8188855564",
+  whatsapp: "91+8188855564", // without +
   email: "info@inspirecollegehm.com",
 };
